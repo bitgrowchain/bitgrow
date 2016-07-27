8d6c24256f7261c3c9285c91dfa9867b0e247f5c
bitgrow <bitgrow>
2016-07-27 10:30:02 -0400

    pow reward adjusted

--- src/main.h
+++ src/main.h
@@ -34,8 +34,8 @@ static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = 100;
 static const int64 MIN_TX_FEE = CENT;
 static const int64 MIN_RELAY_TX_FEE = CENT;
-static const int64 MAX_MONEY = 80000000 * COIN;
-static const int64 MAX_MINT_PROOF_OF_WORK = 500 * COIN;
+static const int64 MAX_MONEY = 44000000 * COIN;
+static const int64 MAX_MINT_PROOF_OF_WORK = 100 * COIN;
 static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY_PPC = 500;
